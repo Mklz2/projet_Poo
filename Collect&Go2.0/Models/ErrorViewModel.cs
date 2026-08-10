5edb9aab96cd1af53e2c6c3dad60cad34b6b39a2
melvin <melvingateu@gmail.com>
2026-08-10 11:24:05 +0200

    Ignore .vs folder

---
+++ Collect&Go2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Collect_Go2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
